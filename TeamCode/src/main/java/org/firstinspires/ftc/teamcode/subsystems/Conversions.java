efe6abd13c4c85f488e31d6fa7e8d902000060fb
hmoose-ash <142426250+hmoose-ash@users.noreply.github.com>
2024-01-16 11:46:08 +0100

    Update motor controller
    now gives max power for small vectors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Conversions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Conversions.java
@@ -2,11 +2,6 @@ package org.firstinspires.ftc.teamcode.subsystems;
 
 public class Conversions {
     public double booleanToDouble(boolean convertee){
-        if (convertee){
-            return 1.0;
-        } else {
-            return 0;
-        }
-
+        return (convertee ? 1.0: 0.0);
     }
 }
